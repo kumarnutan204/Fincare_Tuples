e83a4c46e10a4909ce63fac6dc3533f23e4be075
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-08 12:27:11 +0530

    [Nutan] Added: unpacking of tuples to variables

---
+++ unpack.py
@@ -0,0 +1,9 @@
+a = ("MNNIT Allahabad", 5000, "Engineering")  
+# this lines UNPACKS values : left hand values should be equal to right hand side values
+
+(college, student, type_ofcollege) = a  
+ 
+print(college)
+print(student)
+print(type_ofcollege)
+
